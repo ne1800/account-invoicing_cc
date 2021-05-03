8b14b7a12c50596c6d53151ed7c7f6e31b4b9e7c
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-05-03 13:29:04 +0200

    Fix linting

--- account_invoice_mode_at_shipping/models/stock_picking.py
+++ account_invoice_mode_at_shipping/models/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, _
+from odoo import _, models
 
 from odoo.addons.queue_job.job import job
 
