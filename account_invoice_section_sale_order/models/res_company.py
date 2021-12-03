b0c9de524f135fc9b0ba6f82546335018c63394c
Akim Juillerat <akim.juillerat@camptocamp.com>
2021-12-03 12:54:32 +0100

    Add security group and views

--- account_invoice_section_sale_order/models/res_company.py
+++ account_invoice_section_sale_order/models/res_company.py
@@ -7,14 +7,17 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     invoice_section_name_scheme = fields.Char(
-        help="This is the name of the sections on invoices. Keep empty to use "
-             "default. You can use a python expression with the 'object' "
-             "(representing sale order) and 'time' variables."
+        help="This is the name of the sections on invoices when generated from "
+             "sales orders. Keep empty to use default. You can use a python "
+             "expression with the 'object' (representing sale order) and 'time'"
+             " variables."
     )
 
     invoice_section_grouping = fields.Selection(
         [
             ("sale_order", "Group by sale Order"),
         ],
-        default="sale_order"
+        help="Defines object used to group invoice lines",
+        default="sale_order",
+        required=True,
     )
--- account_invoice_section_sale_order/models/res_partner.py
+++ account_invoice_section_sale_order/models/res_partner.py
@@ -7,7 +7,8 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     invoice_section_name_scheme = fields.Char(
-        help="This is the name of the sections on invoices. Keep empty to use "
-             "default. You can use a python expression with the 'object' "
-             "(representing sale order) and 'time' variables."
+        help="This is the name of the sections on invoices when generated from "
+             "sales orders. Keep empty to use default. You can use a python "
+             "expression with the 'object' (representing sale order) and 'time'"
+             " variables."
     )
