d5c8148054b281bc11a109e8a0fefe2d049107cc
Akim Juillerat <akim.juillerat@camptocamp.com>
2021-12-22 11:38:52 +0100

    Extend tests with mock functions

--- account_invoice_section_sale_order/tests/test_invoice_group_by_sale_order.py
+++ account_invoice_section_sale_order/tests/test_invoice_group_by_sale_order.py
@@ -1,6 +1,8 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+import mock
 
+from odoo.exceptions import UserError
 from odoo.tests.common import SavepointCase
 
 
@@ -113,3 +115,57 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
             lambda r: r.display_type == "line_section"
         )
         self.assertEqual(len(line_sections), 0)
+
+    def test_unknown_invoice_section_grouping_value(self):
+        """Check an error is raised when invoice_section_grouping value is
+        unknown
+        """
+        mock_company_section_grouping = mock.patch.object(
+            type(self.env.company),
+            "invoice_section_grouping",
+            new_callable=mock.PropertyMock,
+        )
+        with mock_company_section_grouping as mocked_company_section_grouping:
+            mocked_company_section_grouping.return_value = "unknown"
+            with self.assertRaises(UserError):
+                (self.order1_p1 + self.order2_p1)._create_invoices()
+
+    def test_custom_grouping_by_sale_order_user(self):
+        """Check custom grouping by sale order user.
+
+        By mocking account.move.line_get_section_grouping and creating
+        res.users.get_invoice_section_name, this test ensures custom grouping
+        is possible by redefining these functions"""
+        demo_user = self.env.ref("base.user_demo")
+        admin_user = self.env.ref("base.partner_admin")
+        orders = self.order1_p1 + self.order2_p1
+        orders.write({"user_id": admin_user.id})
+        sale_order_3 = self.order1_p1.copy({"user_id": demo_user.id})
+        sale_order_3.order_line[0].name = "order 3 line 1"
+        sale_order_3.order_line[1].name = "order 3 line 2"
+        sale_order_3.action_confirm()
+        section_grouping_func = "odoo.addons.account_invoice_section_sale_order.models.account_move.AccountMoveLine._get_section_grouping"  # noqa
+        section_name_func = (
+            "odoo.addons.base.models.res_users.Users._get_invoice_section_name"
+        )
+        with mock.patch(
+            section_grouping_func
+        ) as mocked_get_section_grouping, mock.patch(
+            section_name_func, create=True
+        ) as mocked_get_invoice_section_name:
+            mocked_get_section_grouping.return_value = "sale_line_ids.order_id.user_id"
+            mocked_get_invoice_section_name.return_value = "Mocked value from ResUsers"
+            invoice = (orders + sale_order_3)._create_invoices()
+            result = {
+                10: ("Mocked value from ResUsers", "line_section"),
+                20: ("order 1 line 1", False),
+                30: ("order 1 line 2", False),
+                40: ("order 2 line 1", False),
+                50: ("order 2 line 2", False),
+                60: ("Mocked value from ResUsers", "line_section"),
+                70: ("order 3 line 1", False),
+                80: ("order 3 line 2", False),
+            }
+            for line in invoice.invoice_line_ids.sorted("sequence"):
+                self.assertEqual(line.name, result[line.sequence][0])
+                self.assertEqual(line.display_type, result[line.sequence][1])
